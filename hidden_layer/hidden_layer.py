6458602045cf7c7bb7faa5bfa4016b5aae6216eb
louisld <louisldthesamui@gmail.com>
2022-06-26 23:04:58 +0200

    hidden layer

---
+++ hidden_layer/hidden_layer.py
@@ -0,0 +1,73 @@
+from pickletools import optimize
+import torch
+from torch import nn
+import numpy as np
+import pickle
+import math
+import matplotlib.pyplot as plt
+import gzip
+
+from neuralnetwork import NeuralNetwork
+
+# Utilisation du GPU si disponible
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# Chargement du jeu de données
+fp = gzip.open('../fashion-mnist.pk.gz', 'rb')
+allXtrain, allYtrain, Xtest, Ytest, classlist  = pickle.load(fp) 
+
+Xtrain, Ytrain  = allXtrain[:20000].to(device), allYtrain[:20000].to(device)
+Xvalid, Yvalid  = allXtrain[20000:30000].to(device), allYtrain[20000:30000].to(device)
+
+# Initialisation du réseau neuronal
+model = NeuralNetwork().to(device)
+
+def train_loop(model, loss_fn, optimizer):
+    """
+    Boucle d'entraînement de l'algorithme
+    avec utilisation de mini-batch.
+    """
+    Ntrain = Xtrain.shape[0]
+    Nvalid = Xvalid.shape[0]
+
+    idx = np.arange(Ntrain)
+    batch_size = 200
+    nbatch = int(Ntrain/batch_size)
+
+    np.random.shuffle(idx)
+    l = 0
+    for bi in range(nbatch):
+        ids = idx[bi*batch_size:(bi+1)*batch_size]
+        images = Xtrain[ids]
+        labels = Ytrain[ids]
+
+        # Compute
+        logprobs = model(images)
+        loss = loss_fn(logprobs, labels)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        l += loss.item()
+
+    return l/nbatch
+
+# Initialisation de la fonction de perte et de l'optimisateur
+loss_fn = nn.NLLLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+# Boucle de l'algorithme
+epochs = 50
+train_losses = []
+
+for t in range(epochs):
+    print(f"Epoch {t+1}\n---------------")
+    train_losses.append(train_loop(model, loss_fn, optimizer))
+print("Done !")
+
+plt.plot(train_losses)
+plt.xlabel("epochs")
+plt.ylabel("Pertes")
+plt.show()
---
+++ hidden_layer/neuralnetwork.py
@@ -0,0 +1,20 @@
+from torch import nn
+
+
+class NeuralNetwork(nn.Module):
+    """
+    Réseau neuronal pour apprentissage incrémental.
+    """
+    
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.sequence = nn.Sequential(
+            nn.Linear(28*28, 100),
+            nn.ReLU(),
+            nn.Linear(100, 10),
+            nn.LogSoftmax(dim=1)
+        )
+
+    def forward(self, x):
+        logits = self.sequence(x)
+        return logits
